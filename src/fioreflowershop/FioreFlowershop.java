db7fe94c82abfd04e476d60e5c0239f05e5f2a49
ASUS <ASUS@DESKTOP-VQ8M82U>
2018-11-28 15:17:52 +0800

    28-11-2018 Latest Version Class

--- src/fioreflowershop/FioreFlowershop.java
+++ src/fioreflowershop/FioreFlowershop.java
@@ -5,6 +5,10 @@
  */
 package fioreflowershop;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
 /**
  *
  * @author User
@@ -14,8 +18,24 @@ public class FioreFlowershop {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        //Product Type Data
+        ProductType prodType = new ProductType();
+        List<ProductType> prodTypeList = new ArrayList<ProductType>();
+        prodTypeList.add(new ProductType("PT1001","Fresh Flowers","The flowers that will be wither..."));
+        prodTypeList.add(new ProductType("PT1002","Bouquets","An attractively arranged bunch of flowers, especially one presented as a gift or carried at a ceremony."));
+        prodTypeList.add(new ProductType("PT1003","Floral Arrangements","Flower arrangement is an organization of design and color towards creating an ambience using flowers, foliage and other floral accessories"));
+        
+        //Product Data
+        Product prod = new Product();
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(new Product("P1001","Flowers and Chocolates Gift","DESC...","Red",60.00,5,prodTypeList.get(0)));
+        productList.add(new Product("P1002","Ladies Gift Hamper","DESC...","Red",65.00,5,prodTypeList.get(1)));
+        productList.add(new Product("P1003","Christmas Treats Gift Box","DESC...","White",15.00,5,prodTypeList.get(1)));
+        productList.add(new Product("P1004","Honeybee","DESC...","Yellow",30.00,5,prodTypeList.get(0)));
+        productList.add(new Product("P1005","Starry Night","DESC...","Blue",45.00,5,prodTypeList.get(2)));
+        
+        System.out.println("Main Program");
     }
     
 }
