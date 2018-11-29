c6337dc3c904b2bf57d4c102a345d2e18b360eb5
User <User@LAPTOP-VF1KBQUV>
2018-11-30 00:55:16 +0800

    Update 3 generate sales order

--- src/catalogOrder/GenerateSalesOrder.java
+++ src/catalogOrder/GenerateSalesOrder.java
@@ -20,30 +20,35 @@ import java.util.Scanner;
 public class GenerateSalesOrder {
     public static List<OrderList> GenerateReportMain(List<ProductType> prodTypeList, List<Product> prodList, List<Order> orderDataList, List <OrderList> orderList ) throws IOException{    
        Scanner scan = new Scanner(System.in); 
-       
+       int sumQty = 0;
+       double totalAmount = 0;
        System.out.print("Do you want to generate report ? (Y/N) : ");
        String selectionReport = scan.next();
-       for(int i=0; i<orderList.size(); i++){
+       
         System.out.println("==========Product Type===========");
-        
-        for(int r=0; r<prodTypeList.size();r++)
-        {
-            System.out.println(String.format("%d. %s", r + 1, prodTypeList.get(r).getProductTypeName()));
-            
-        }
+        for(int i=0; i<prodTypeList.size(); i++){
+            System.out.println(String.format("%d. %s", i + 1, prodTypeList.get(i).getProductTypeName()));
+       }
         
         System.out.print("Please enter product type : ");
         int selectionProdType = scan.nextInt(); 
         
-        if(orderList.get(i).getProduct().getProductType().equals(prodTypeList.get(prodTypeList.size()-1).getProductTypeID()))
-        {
-            //System.out.println();
+         ProductType productType = new ProductType();
+        
+        productType = prodTypeList.get(selectionProdType-1);
+        
+        System.out.println("No. \t\t Product Name \t\t\t Quantity \t Total Amount \t");
+        for(int i=0; i<orderList.size();i++){
+            if(orderList.get(i).getProduct().getProductType().equals(productType))
+            {
+                totalAmount = orderList.get(i).getProduct().getProductPrice()*orderList.get(i).getQuantity();
+                System.out.println(String.format("%d.  \t %s %s %.2f %d %.2f", i + 1,orderList.get(i).getProduct().getProductID(), orderList.get(i).getProduct().getProductName(), orderList.get(i).getProduct().getProductPrice(),orderList.get(i).getQuantity(), totalAmount ));
+                sumQty += orderList.get(i).getQuantity();
+            }
         }
         
-       }
-        
-
+        System.out.print("Total Quantity : " + sumQty);
         return orderList;
-    }
 
+    }
 }
